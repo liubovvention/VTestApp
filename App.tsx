6a5b985ae0935afc89a74e74cbb4d27454991ac7
liubovvention <liubov.kuskova@ventionteams.com>
2024-09-23 14:45:51 +0300

    Initial commit

--- App.tsx
+++ App.tsx
@@ -6,30 +6,26 @@
  */
 
 import React from 'react';
-import type {PropsWithChildren} from 'react';
+import type { PropsWithChildren } from 'react';
 import {
   SafeAreaView,
-  ScrollView,
   StatusBar,
   StyleSheet,
   Text,
   useColorScheme,
   View,
+  Dimensions,
 } from 'react-native';
 
 import {
   Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
 type SectionProps = PropsWithChildren<{
   title: string;
 }>;
 
-function Section({children, title}: SectionProps): React.JSX.Element {
+function Section({ children, title }: SectionProps): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
   return (
     <View style={styles.sectionContainer}>
@@ -68,38 +64,29 @@ function App(): React.JSX.Element {
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+      <View
+        style={[styles.viewContainer, {
+          backgroundColor: backgroundStyle.backgroundColor,
+        }]}>
+        <Section title="Here We Go Again">
+          It will be some test app
+        </Section>
+      </View>
     </SafeAreaView>
   );
 }
 
+const { width, height } = Dimensions.get('screen')
+
 const styles = StyleSheet.create({
+  viewContainer: {
+    width: width,
+    height: height,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
   sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+    paddingHorizontal: 20,
   },
   sectionTitle: {
     fontSize: 24,
