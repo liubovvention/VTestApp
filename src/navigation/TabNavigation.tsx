4539616a0ebbd3b40ecbc383a3d62959af1df545
liubovvention <liubov.kuskova@ventionteams.com>
2024-09-26 13:05:40 +0300

    wip: styling

--- src/navigation/TabNavigation.tsx
+++ src/navigation/TabNavigation.tsx
@@ -1,4 +1,6 @@
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import SettingsNavigation from 'navigation/SettingsNavigation';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import {HomeScreen, SettingsScreen} from 'screens';
 
 const TabNavigation = () => {
@@ -7,7 +9,22 @@ const TabNavigation = () => {
   return (
     <Tab.Navigator
       initialRouteName="Weather"
-      screenOptions={{headerShown: true}}>
+      screenOptions={({route}) => ({
+        tabBarIcon: () => {
+          let iconName = 'ios-information-circle';
+          if (route.name === 'Weather') {
+            iconName = 'cloud';
+          } else if (route.name === 'Settings') {
+            iconName = 'gear';
+          }
+          return <Icon name={iconName} size={20} color="white" />;
+        },
+        tabBarActiveBackgroundColor: 'blue',
+        tabBarInactiveBackgroundColor: 'gray',
+        tabBarActiveTintColor: 'white',
+        tabBarInactiveTintColor: 'white',
+        headerShown: false,
+      })}>
       <Tab.Screen name="Weather" component={HomeScreen} />
       <Tab.Screen name="Settings" component={SettingsScreen} />
     </Tab.Navigator>
