a8c859f9fc6551b3ae273dea705c7fcb397a7c60
liubovvention <liubov.kuskova@ventionteams.com>
2024-09-26 14:48:45 +0300

    wip: contacts screen styling

--- src/screens/ContactsScreen/ContactsScreen.tsx
+++ src/screens/ContactsScreen/ContactsScreen.tsx
@@ -1,10 +1,50 @@
-import {Text, View} from 'react-native';
+import {useCallback} from 'react';
+import {Button, Linking, Pressable, Text, View} from 'react-native';
 import styles from './ContactsScreenStyles';
 
 export default function ContactsScreen() {
+  const handleEmailPress = useCallback(() => {
+    const email = 'support@example.com';
+    Linking.openURL(`mailto:${email}`);
+  }, []);
+
+  const handlePhonePress = useCallback(() => {
+    const phoneNumber = 'tel:+1234567890';
+    Linking.openURL(phoneNumber);
+  }, []);
+
+  const handleSmsPress = useCallback(() => {
+    const phoneNumber = 'sms:+1234567890';
+    Linking.openURL(phoneNumber);
+  }, []);
+
   return (
     <View style={styles.viewContainer}>
-      <Text>Contacts Screen</Text>
+      <Text style={styles.title}>Contact Our Support Team</Text>
+      <Pressable
+        onPress={handleEmailPress}
+        style={({pressed}) => [
+          styles.button,
+          pressed ? styles.buttonPressed : null,
+        ]}>
+        <Text style={styles.label}>By Email</Text>
+      </Pressable>
+      <Pressable
+        onPress={handlePhonePress}
+        style={({pressed}) => [
+          styles.button,
+          pressed ? styles.buttonPressed : null,
+        ]}>
+        <Text style={styles.label}>By Phone</Text>
+      </Pressable>
+      <Pressable
+        onPress={handleSmsPress}
+        style={({pressed}) => [
+          styles.button,
+          pressed ? styles.buttonPressed : null,
+        ]}>
+        <Text style={styles.label}>Via SMS</Text>
+      </Pressable>
     </View>
   );
 }
--- src/screens/ContactsScreen/ContactsScreenStyles.ts
+++ src/screens/ContactsScreen/ContactsScreenStyles.ts
@@ -9,6 +9,30 @@ const styles = StyleSheet.create({
       justifyContent: 'center',
       alignItems: 'center',
     },
+    title: {
+      fontSize: 24,
+      fontWeight: 'bold',
+      marginBottom: 20,
+    },
+    button: {
+      width: width/2,
+      backgroundColor: '#d3d3d3', 
+      paddingVertical: 12,
+      paddingHorizontal: 20,
+      borderRadius: 10, 
+      marginVertical: 10,
+      justifyContent: 'center',
+      alignItems: 'center',
+      padding: 10,
+    },
+    buttonPressed: {
+      backgroundColor: 'blue', 
+    },
+    label: {
+      fontSize: 14,
+      fontWeight: 'bold',
+      color: 'gray',
+    }
   });
 
   export default styles;
