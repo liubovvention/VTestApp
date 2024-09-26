6e25462ab97d569d561b4817cab2e09e457d21e8
liubovvention <liubov.kuskova@ventionteams.com>
2024-09-26 14:52:35 +0300

    wip: logout

--- src/screens/LogoutScreen/LogoutScreen.tsx
+++ src/screens/LogoutScreen/LogoutScreen.tsx
@@ -2,6 +2,7 @@ import {Text, View} from 'react-native';
 import styles from './LogoutScreenStyles';
 
 export default function LogoutScreen() {
+  //logout logic will be here
   return (
     <View style={styles.viewContainer}>
       <Text>Logout Screen</Text>
