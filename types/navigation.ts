49962d2945d80b072f73c6ab4ee07ccfc0113657
liubovvention <liubov.kuskova@ventionteams.com>
2024-09-26 16:26:56 +0300

    cleanup

--- types/navigation.ts
+++ types/navigation.ts
@@ -1,11 +1,11 @@
-import { CityWeather } from "./weather";
+import {CityWeather} from './weather';
 
 export type StackParamList = {
-    Home: undefined;
-    Weather: undefined;  
-    Details: CityWeather;   // Screen "City Weather" 
-    Settings: undefined;
-    Contacts: undefined;
-    'Log Out': undefined;
-    'App Info': undefined;
-  };
+  Home: undefined;
+  Weather: undefined;
+  Details: CityWeather; // Screen "City Weather"
+  Settings: undefined;
+  Contacts: undefined;
+  'Log Out': undefined;
+  'App Info': undefined;
+};
