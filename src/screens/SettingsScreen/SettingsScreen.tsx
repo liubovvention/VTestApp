53f0df60c5491a7bfcc62a43aada87f098fa2c8f
liubovvention <liubov.kuskova@ventionteams.com>
2024-09-26 09:36:31 +0300

    start task: add settings screen

---
+++ src/screens/SettingsScreen/SettingsScreen.tsx
@@ -0,0 +1,10 @@
+import {Text, View} from 'react-native';
+import styles from './SettingsScreenStyles';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.viewContainer}>
+      <Text>Settings Screen</Text>
+    </View>
+  );
+}
