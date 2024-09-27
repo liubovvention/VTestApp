7bbf56151e5aafdce4eee751f4c0302945e65c1b
liubovvention <liubov.kuskova@ventionteams.com>
2024-09-27 11:04:00 +0300

    wip: contacts handling optimisation

--- src/screens/ContactsScreen/ContactsScreen.tsx
+++ src/screens/ContactsScreen/ContactsScreen.tsx
@@ -1,33 +1,49 @@
 import {useCallback} from 'react';
 import {Linking, Text, View} from 'react-native';
-import { StyledButton } from 'components';
-import { useThemedStyles } from 'styles/commonStyles';
-import styles from './ContactsScreenStyles';
+import {StyledButton} from 'components';
+import {useThemedStyles} from 'styles/commonStyles';
+import styles from 'src/screens/ContactsScreen/ContactsScreenStyles';
 
 export default function ContactsScreen() {
   const themedStyles = useThemedStyles();
-  
-  const handleEmailPress = useCallback(() => {
-    const email = 'support@example.com';
-    Linking.openURL(`mailto:${email}`);
-  }, []);
 
-  const handlePhonePress = useCallback(() => {
-    const phoneNumber = 'tel:+1234567890';
-    Linking.openURL(phoneNumber);
-  }, []);
-
-  const handleSmsPress = useCallback(() => {
-    const phoneNumber = 'sms:+1234567890';
-    Linking.openURL(phoneNumber);
+  const handleButtonPress = useCallback((action: string) => {
+    let linkingAction;
+    switch (action) {
+      case 'email':
+        const email = 'support@example.com';
+        linkingAction = `mailto:${email}`;
+        break;
+      case 'phone':
+        linkingAction = 'tel:+1234567890';
+        break;
+      case 'sms':
+        linkingAction = 'sms:+1234567890';
+        break;
+      default:
+        break;
+    }
+    if (linkingAction) Linking.openURL(linkingAction);
   }, []);
 
   return (
     <View style={themedStyles.container}>
       <Text style={themedStyles.title}>Contact Our Support Team</Text>
-      <StyledButton onPress={handleEmailPress} label='By Email' styles={styles.button} />
-      <StyledButton onPress={handlePhonePress} label='By Phone' styles={styles.button} />
-      <StyledButton onPress={handleSmsPress} label='Via SMS' styles={styles.button} />
+      <StyledButton
+        onPress={() => handleButtonPress('email')}
+        label="By Email"
+        styles={styles.button}
+      />
+      <StyledButton
+        onPress={() => handleButtonPress('phone')}
+        label="By Phone"
+        styles={styles.button}
+      />
+      <StyledButton
+        onPress={() => handleButtonPress('sms')}
+        label="Via SMS"
+        styles={styles.button}
+      />
     </View>
   );
 }
