c6bf3d2c72e4cd2a859ac8a003c4b5e67c958d0b
liubovvention <liubov.kuskova@ventionteams.com>
2024-09-27 16:45:50 +0300

    wip: app info optimised

--- src/screens/InfoScreen/InfoScreen.tsx
+++ src/screens/InfoScreen/InfoScreen.tsx
@@ -1,29 +1,19 @@
-import { useEffect, useState } from 'react';
+import {useEffect, useState} from 'react';
 import DeviceInfo from 'react-native-device-info';
 import {Text, View} from 'react-native';
-import { useThemedStyles } from 'styles/commonStyles';
+import {useThemedStyles} from 'styles/commonStyles';
 import styles from 'src/screens/InfoScreen/InfoScreenStyles';
-
 
 export default function InfoScreen() {
   const themedStyles = useThemedStyles();
-  const [appInfo, setAppInfo] = useState({ version: '', build: '' });
-
-  useEffect(() => {
-    const fetchAppInfo = () => {
-      const version = DeviceInfo.getVersion();
-      const build = DeviceInfo.getBuildNumber();
-      setAppInfo({ version, build });
-    };
-
-    fetchAppInfo();
-  }, []);
+  const version = DeviceInfo.getVersion();
+  const build = DeviceInfo.getBuildNumber();
 
   return (
     <View style={themedStyles.container}>
       <Text style={themedStyles.title}>App Information</Text>
-      <Text style={styles.info}>Version: {appInfo.version}</Text>
-      <Text style={styles.info}>Build: {appInfo.build}</Text>
+      <Text style={styles.info}>Version: {version}</Text>
+      <Text style={styles.info}>Build: {build}</Text>
     </View>
   );
 }
