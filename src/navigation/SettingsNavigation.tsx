ce0bfca21f6eaea3f418ce79ca4d2af456539c7a
liubovvention <liubov.kuskova@ventionteams.com>
2024-09-30 12:45:43 +0300

    wip: handle settings options

--- src/navigation/SettingsNavigation.tsx
+++ src/navigation/SettingsNavigation.tsx
@@ -1,21 +1,47 @@
+import {useEffect, useState} from 'react';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import {
   SettingsScreen,
   InfoScreen,
   LogoutScreen,
   ContactsScreen,
+  LoginScreen,
 } from 'screens';
-import { ScreenNames } from 'types/navigation';
+import {useAppSelector} from 'hooks/useStore';
+import {selectisLoggedIn, selectUser} from 'store/slices/auth/authSlice';
+import {ScreenNames} from 'types/navigation';
 
 const Drawer = createDrawerNavigator();
 
 const SettingsNavigation = () => {
+  const user = useAppSelector(selectUser);
+  const isStoredLoggedIn = useAppSelector(selectisLoggedIn);
+  //const [isLoading, setIsLoading] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const checkLoginStatus = async () => {
+      if (isStoredLoggedIn && user) {
+        setIsLoggedIn(true);
+      }
+      //setIsLoading(false);
+    };
+
+    checkLoginStatus();
+  }, [isStoredLoggedIn, user]);
   return (
     <Drawer.Navigator initialRouteName={ScreenNames.Settings}>
-      <Drawer.Screen name={ScreenNames.GeneralSettings} component={SettingsScreen} />
+      <Drawer.Screen
+        name={ScreenNames.GeneralSettings}
+        component={SettingsScreen}
+      />
       <Drawer.Screen name={ScreenNames.Info} component={InfoScreen} />
       <Drawer.Screen name={ScreenNames.Contacts} component={ContactsScreen} />
-      <Drawer.Screen name={ScreenNames.Logout} component={LogoutScreen} />
+      {isLoggedIn ? (
+        <Drawer.Screen name={ScreenNames.Logout} component={LogoutScreen} />
+      ) : (
+        <Drawer.Screen name={ScreenNames.Login} component={LoginScreen} />
+      )}
     </Drawer.Navigator>
   );
 };
