e609d5d5b64effd9b80c806d688cad111695ee7d
liubovvention <liubov.kuskova@ventionteams.com>
2024-09-30 12:41:09 +0300

    cleanup

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,13 +1,9 @@
-import {combineReducers, UnknownAction} from '@reduxjs/toolkit';
+import {combineReducers} from '@reduxjs/toolkit';
 import authSlice from 'store/slices/auth/authSlice';
 import {SLICES} from 'types/store';
 
-const combinedReducers = combineReducers({
+const rootReducer = combineReducers({
   [SLICES.AUTH]: authSlice,
 });
-
-const rootReducer = (state: any, action: UnknownAction) => {
-  return combinedReducers(state, action);
-};
 
 export default rootReducer;
